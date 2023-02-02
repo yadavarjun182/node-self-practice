dd86bacf417b5635f6cf7eeb194fe0b21935cba0
Arjun Yadav <yadavarjun182@gmail.com>
2023-02-02 23:10:19 +0530

    express learnings

---
+++ day9/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const fs = require("fs")
+
+const app = express()
+
+app.use((req, res, next) => {
+    const startTime = new Date().getTime()
+    next()
+    const endTime = new Date().getTime()
+    fs.appendFileSync("./routedetails", `route visited: ${req.url} | method: ${rq.method} | Response time:  ${endTime - startTime}`)
+    console.log("middleware")
+    next()
+    console.log("bye middleware")
+})
+
+app.get("/", (req, res) => {
+    console.log("home page")
+    res.send("home page")
+})
+
+app.get("/about", (req, res) => {
+    console.log("home page")
+    res.send("about page")
+})
