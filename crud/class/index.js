5b514cc149f6274c87783ee64b2a97e7bd74ebd7
Arjun Yadav <yadavarjun182@gmail.com>
2023-02-04 22:47:28 +0530

    iitial commit

---
+++ crud/class/index.js
@@ -0,0 +1,18 @@
+const express=require("express")
+const app=express()
+//this is a middleware we will see these in detail in the upcoming session
+app.use(express.json()) //this will parse the data in the req.body and you will be able to get it as well and console.log() it
+app.get("/", (req,res)=>{
+res.send("Hello")
+})
+app.post("/adddetails",(req,res)=>{
+console.log(req.body)
+res.send("data has been accepted")
+})
+//to send all the details of the students that are added
+app.get("/details", (req,res)=>{
+res.send("All details so far...")
+})
+app.listen(4500,()=>{
+console.log("running on port 4500")
+})
