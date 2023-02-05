7fb408a17cf6d48be8d9f25b992b37f11961249e
Arjun Yadav <yadavarjun182@gmail.com>
2023-02-05 10:44:40 +0530

    diff req diff res

---
+++ http_practice_once_again/index.js
@@ -0,0 +1,18 @@
+//creating  different responses for different requests
+const http = require("http")
+const PORT = 8000
+const server = http.createServer((req, res) => {
+    if (req.url == "/") {
+        res.end("Hello Welcome to Homepage")
+    }
+    else if (req.url == '/reports') {
+        res.end("Here are all the reports generated so far")
+    }
+    else if (req.url == "/data") {
+        res.end("all data.......................")
+    }
+})
+
+server.listen(`${PORT}`, () => {
+    console.log(`server listening on port ${PORT}`)
+})
