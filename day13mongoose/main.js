cf7c024951b4fd70be861ef150afba81055fd13d
Arjun Yadav <yadavarjun182@gmail.com>
2023-02-07 23:29:35 +0530

    update city field to bangalore

--- day13mongoose/main.js
+++ day13mongoose/main.js
@@ -4,18 +4,33 @@ const main = async () => {
     const connection = await mongoose.connect("mongodb://127.0.0.1:27017/backend")
     console.log("connected to mongo")
     // await StudentModel.insertMany([{ name: "arjun", age: 26, city: "pune" }])
+    // await StudentModel.insertMany([{ name: "arjun", age: 26, city: "pune" },
+    // {
+    //     name: "bbb", age: 26, city: "pune"
+    // },
+    // {
+    //     name: "ccc", age: 26, city: "pune"
+    // },
+    // { name: "ddd", age: 26, city: "pune" },
+    // { name: "eee", age: 26, city: "pune" },
+    // { name: "ffff", age: 26, city: "pune" },
+    // { name: "gggg", age: 26, city: "pune" }
+    // ])
 
 
-    const student = new StudentModel(
-        {
-            name: "arnab",
-            age: 26,
-            city: "pune"
-        })
-    await student.save()
+    // const student = new StudentModel(
+    //     {
+    //         name: "arnab",
+    //         age: 26,
+    //         city: "pune"
+    //     })
+    // await student.save()
 
 
-    const data=await StudentModel.find()
+   await StudentModel.updateMany({"city":"pune"},{$set:{"city":"bangalore"}})
+    const data = await StudentModel.find()
+
+
     console.log(data)
     connection.disconnect()
     console.log("disconnected")
