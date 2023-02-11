715152b8f596f2a2d5e0612ef52455dc65538bd2
Arjun Yadav <yadavarjun182@gmail.com>
2023-02-11 23:30:02 +0530

    test multer

--- day17/index.js
+++ day17/index.js
@@ -1,13 +1,25 @@
-const express = require("express")
+const express=require("express")
 const multer = require("multer")
 const app = express()
 
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, "uploads")
+        },
+        filename: (req, file, cb) => {
+            cb(null, file.filename + "-" + Date.now() + ".jpg")
+        }
+    })
+}).single("user_file")
 
-app.post("/upload", (req, res) => {
+
+
+app.post("/upload", upload, (req, res) => {
     res.send("file uploaded")
 })
 
 
-app.listen(9001,()=>{
-console.log("server listening at 9001")
+app.listen(9001, () => {
+    console.log("server listening at 9001")
 })
