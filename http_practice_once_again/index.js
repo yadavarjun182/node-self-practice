ef8472860edde4637df2ef4c9f87fb449a13ffd6
Arjun Yadav <yadavarjun182@gmail.com>
2023-02-17 23:05:27 +0530

    data route updated

--- http_practice_once_again/index.js
+++ http_practice_once_again/index.js
@@ -3,13 +3,10 @@ const http = require("http")
 const fs = require("fs")
 const PORT = 8000
 
-
-
-
 const server = http.createServer((req, res) => {
     if (req.url == "/") {
         res.setHeader("Content-Type", "text/html") //header to specify the res is in html form
-        res.end("Hello Welcome to Homepage")
+        res.end("Hello Welcome to Homepage ")
     }
     else if (req.url == '/reports') {
         res.end("Here are all the reports generated so far")
@@ -17,19 +14,17 @@ const server = http.createServer((req, res) => {
     else if (req.url == "/data") {
         fs.readFile("./text.txt", "utf-8", (err, data) => {
             if (err) {
-                res.write("no data present")
+                res.write("err occured and no data present")
                 res.end(err)
             } else {
                 res.end(data)
             }
-
         })
     }
     else {
         res.end("bad request")
     }
 })
-
 server.listen(`${PORT}`, () => {
     console.log(`server listening on port ${PORT}`)
 })
